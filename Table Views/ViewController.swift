21998ea7b27f090f433cf9934c68f87d4ec99f94
Nathanial L. McConnell <FluxAugur@gmail.com>
2014-10-05 21:22:41 -0400

    prepped for GitHub push

--- Table Views/ViewController.swift
+++ Table Views/ViewController.swift
@@ -8,8 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+  var array = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"]
+  
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+    return array.count
+  }
+  
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    var cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+    
+    cell?.textLabel?.text = array[indexPath.row]
+    
+    return cell!
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
